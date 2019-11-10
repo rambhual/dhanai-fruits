88d5bd333687144233cadb63f1135a34cb559d5a
Ramprit Sahani <rampritsahani@protonmail.com>
2019-11-10 08:31:07 +0530

    remove css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss";
-import "assets/demo/demo.css";
-import "assets/demo/nucleo-icons-page-styles.css";
+import "assets/now/now.css";
+import "assets/now/nucleo.css";
 // pages for this kit
 
 import ApolloProvider from "./ApolloProvider";
